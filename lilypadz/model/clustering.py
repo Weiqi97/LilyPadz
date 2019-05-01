38e42d6b2bf01e3a1bcc9ad016709a8b650db5b6
Weiqi Feng <weltch1997@gmail.com>
2019-04-30 22:26:08 -0400

    Done

--- lilypadz/model/clustering.py
+++ lilypadz/model/clustering.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 from typing import List
-
 from flask import jsonify
 from plotly.offline import plot
 from sklearn.cluster import KMeans
